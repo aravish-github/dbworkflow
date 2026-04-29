d8bf9768249a14d7dcb22df817da4f703b9df2cc
aravish-github <123108151+aravish-github@users.noreply.github.com>
2026-04-29 18:35:38 +0530

    Comment out setup and import section
    Comment out import statements and current directory print.

--- level1/level2/level3/level4/level5/databricks_notebook.py
+++ level1/level2/level3/level4/level5/databricks_notebook.py
@@ -14,10 +14,10 @@
 
 # COMMAND ----------
 
-import os
-import sys
-current_dir = os.path.dirname(os.path.abspath(__file__))
-print(current_dir)
+# import os
+# import sys
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# print(current_dir)
 # if current_dir not in sys.path:
     # sys.path.insert(0, current_dir)
 
