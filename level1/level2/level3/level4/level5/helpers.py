01c703717543f1eacb31f48cfcc49e53c5bb5011
aravish-github <123108151+aravish-github@users.noreply.github.com>
2026-05-12 13:45:08 +0530

    Disable repo_root path insertion in helpers.py
    Comment out repo_root path manipulation code.

--- level1/level2/level3/level4/level5/helpers.py
+++ level1/level2/level3/level4/level5/helpers.py
@@ -7,9 +7,9 @@ from typing import List, Dict, Any
 import sys
 import os
 
-repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../'))
-if repo_root not in sys.path:
-  sys.path.insert(0, repo_root)
+# repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../'))
+# if repo_root not in sys.path:
+#   sys.path.insert(0, repo_root)
 
 class Helpers:
     """
